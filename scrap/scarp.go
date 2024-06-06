fd958695bda0ca3b443e7546abc6a4e0345fbed5
MUSTAFA-A-KHAN <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2024-06-06 11:40:35 +0000

    feat: add functionality to scrap

--- scrap/scarp.go
+++ scrap/scarp.go
@@ -1,1 +1,35 @@
 package scrap
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func scrapperService(url string) {
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(url),
+	)
+
+	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+		link := h.Attr("href")
+		fmt.Print("link:", link)
+		h.Request.Visit(link)
+	})
+
+	c.OnHTML("title", func(h *colly.HTMLElement) {
+		fmt.Println("page title:", h.Text)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting:", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		println("error")
+	})
+
+	c.Visit("url")
+
+}
